8b51fae0520764ece5a02ce5822543aa563f1862
Daniel <decrdesign@gmail.com>
2020-05-03 02:48:49 -0400

    gradient

--- frontend/src/components/sidebar.js
+++ frontend/src/components/sidebar.js
@@ -4,6 +4,7 @@ import { space, layout, flexbox } from "styled-system"
 const Sidebar = styled.aside`
   display: flex;
   background-color: ${({ theme }) => theme.colors.primary};
+  background: ${({ theme }) => `linear-gradient(135deg, ${theme.colors.info} 0%, ${theme.colors.primary} 100%)`};
   flex: 0 1 230px;
   transition: all ease 0.5s;
   padding: ${({ theme }) => theme.space.medium}px;
